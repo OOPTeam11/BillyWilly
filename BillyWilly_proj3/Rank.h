d841cab28e30012ccdba8798625c3629e9602cb9
Jo Mingyu <owlsogul@gmail.com>
2017-11-15 03:46:29 +0900

    점수를 저장할 Rank 클래스 인터페이스 및 구현
    간단한 클래스이므로 따로 분리하지 않았다.

---
+++ BillyWilly_proj3/Rank.h
@@ -0,0 +1,22 @@
+#include <string>
+
+using namespace std;
+
+class Rank{
+public:
+	Rank(string name, int mode, int score){
+		this->name = name;
+		this->mode = mode;
+		this->score = score;
+	};
+	string name;
+	int mode;
+	int score;
+	//for sort. define operator.
+	friend bool operator>(const Rank& a, const Rank& b){
+		return a.score > b.score;
+	}
+	friend bool operator<(const Rank& a, const Rank& b){
+		return a.score < b.score;
+	}
+};
