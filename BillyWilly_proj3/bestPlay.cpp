deaf830db2033b0f2aa3f874df54b07fe148e4a2
whd0714 <jonghyun0714@gmail.com>
2017-11-19 20:31:31 +0900

    최고의 플레이 - 필요없는 함수 삭제

--- BillyWilly_proj3/bestPlay.cpp
+++ BillyWilly_proj3/bestPlay.cpp
@@ -73,51 +73,6 @@ void bestPlay::showStartPos(IDirect3DDevice9** Device, D3DXMATRIX* g_mWorld, CSp
 	}
 }
 
-void bestPlay::showReplay(IDirect3DDevice9** Device, D3DXMATRIX* g_mWorld, CSphere	*g_sphere, CWall *g_legowall, CWall *g_legoPlane, CSphere *g_target_blueball, CLight *g_light) {
-
-	for (int i = 0; i < 4; i++) {
-		g_sphere[i] = this->cur_g_sphere[i];
-		g_legowall[i] = this->cur_g_legowall[i];
-	}
-	*g_legoPlane = this->cur_g_legoPlane;
-	*g_target_blueball = this->cur_g_target_blueball;
-	*g_light = this->cur_g_light;
-
-	if (*Device)
-	{
-		(*Device)->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0x00afafaf, 1.0f, 0);
-		(*Device)->BeginScene();
-
-		// update the position of each ball. during update, check whether each ball hit by walls.
-		for (int i = 0; i < 4; i++) {
-			g_sphere[i].ballUpdate(curTimeDelta);
-			for (int j = 0; j < 4; j++){ g_legowall[i].hitBy(g_sphere[j]); }
-		}
-
-		// check whether any two balls hit together and update the direction of balls
-		for (int i = 0; i < 4; i++){
-			for (int j = 0; j < 4; j++) {
-				if (i < j)
-					g_sphere[i].hitBy(g_sphere[j]);
-			}
-		}
-
-		// draw plane, walls, and spheres
-		(*g_legoPlane).draw(*Device, *g_mWorld);
-		for (int i = 0; i<4; i++) 	{
-			g_legowall[i].draw(*Device, *g_mWorld);
-			g_sphere[i].draw(*Device, *g_mWorld);
-		}
-		(*g_target_blueball).draw(*Device, *g_mWorld);
-		(*g_light).draw(*Device);
-
-		(*Device)->EndScene();
-		(*Device)->Present(0, 0, 0, 0);
-		(*Device)->SetTexture(0, NULL);
-	}
-	
-}
-
 bool bestPlay::threeCushion() {
 	int redBallCount = 0;
 	int wallCount = 0;
@@ -129,7 +84,7 @@ bool bestPlay::threeCushion() {
 			wallCount++;
 		}
 		if (redBallCount == 2) {
-			if (wallCount < 1) {
+			if (wallCount < 3) {
 				return false;
 			}
 			else return true;
--- BillyWilly_proj3/bestPlay.h
+++ BillyWilly_proj3/bestPlay.h
@@ -11,34 +11,32 @@ using namespace std;
 #define WHITEBALL 3
 #define WALL 5
 
+/*
+쓰리쿠션의 순간을 판단하여 이 경우 게임 플레이 장면을 다시 보여주는 기능을 담은 함수이다.
+*/
+
 class bestPlay{
 public:
 	bestPlay();
 	//
 	~bestPlay();
-	void saveLastStatus(float timeDelta, CSphere *g_sphere, CWall *g_legowall, CWall g_legoPlane, CSphere g_target_blueball, CLight g_light); // 스페이스바가 눌리면 출발할 때의 4개의 공 상태 저장.
-	void savePower(float m_velocity_x, float m_velocity_z); //
-	float getVelocity_X(); //
-	float getVelocity_Z();
-
-	void getLastStatus(CSphere	*g_sphere, CWall *g_legowall, CWall *g_legoPlane, CSphere *g_target_blueball, CLight *g_light);
-	void getCurStatus(CSphere	*g_sphere, CWall *g_legowall, CWall *g_legoPlane, CSphere *g_target_blueball, CLight *g_light);
+	void saveLastStatus(float timeDelta, CSphere *g_sphere, CWall *g_legowall, CWall g_legoPlane, CSphere g_target_blueball, CLight g_light); // 출발위치로 공을 되돌리기 위해 현재 공, 벽의 상태를 저장
+	void savePower(float m_velocity_x, float m_velocity_z); // 출발하는 공의 x축, z축 velocity값을 저장
+	float getVelocity_X(); // m_velocity_x 값을 반환
+	float getVelocity_Z(); // m_velocity_z 값을 반환
 	void showStartPos(IDirect3DDevice9** Device, D3DXMATRIX* g_mWorld,
-					CSphere	*g_sphere, CWall *g_legowall, CWall *g_legoPlane, CSphere *g_target_blueball, CLight *g_light);
-	void showReplay(IDirect3DDevice9** Device, D3DXMATRIX* g_mWorld,
-					CSphere	*g_sphere, CWall *g_legowall, CWall *g_legoPlane, CSphere *g_target_blueball, CLight *g_light);
-	bool threeCushion();
+					CSphere	*g_sphere, CWall *g_legowall, CWall *g_legoPlane, CSphere *g_target_blueball, CLight *g_light); // 현재 공 상태를 저장된 공 상태로 바꾸고 화면에 출력
+	bool threeCushion(); // 쓰리쿠션 상황판단
 	vector<int> cusionCount;
 private:
 	float lastTimeDelta;
-	float curTimeDelta;
 	float m_velocity_x;
 	float m_velocity_z;
-	CSphere last_g_sphere[4], cur_g_sphere[4];
+	CSphere last_g_sphere[4];
 	CSphere	last_g_target_blueball, cur_g_target_blueball;
-	CWall	last_g_legoPlane, cur_g_legoPlane;
-	CWall	last_g_legowall[4], cur_g_legowall[4];
-	CLight	last_g_light, cur_g_light;
+	CWall	last_g_legoPlane;
+	CWall	last_g_legowall[4];
+	CLight	last_g_light;
 	CSphere * currentBall;
 
 };
